6033b66ecd67817774bef97982fa936f2329b75a
Shini084 <79849892+shini084@users.noreply.github.com>
2023-01-30 18:40:55 -0300

    Update ex008.js

--- exercicíos/ex008.js
+++ exercicíos/ex008.js
@@ -1,1 +1,8 @@
-console.log('o cosole funcionou corretamente')
+var vel = 60.5
+console.log(`a velocidade do seu carro é de ${vel}Km/h`)
+
+if (vel > 60){
+    console.log(`você ultrapassou a velocidade permitida. Você foi Multado!`)
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
