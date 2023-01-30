7f23ebdaedc890d92c1d83e54de36954c3437790
Shini084 <79849892+shini084@users.noreply.github.com>
2023-01-30 18:46:19 -0300

    Create ex009.js

---
+++ exercicíos/ex009.js
@@ -0,0 +1,9 @@
+var nacionalidade = 'brasileiro'
+
+console.log(`sendo um ${nacionalidade} você é:`)
+
+if (nacionalidade == 'brasileiro'){
+    console.log(`Nativo!`)
+} else{
+    console.log(`Extrangeiro!`)
+}
