9b0d251a516287d0bda5237f0c6f5e67275d70a6
Shini084 <79849892+shini084@users.noreply.github.com>
2023-02-05 20:58:25 -0300

    -

---
+++ exercicíos/ex018/ex018-scripty.js
@@ -0,0 +1,8 @@
+function contar(){
+    var inicio = window.document.querySelector('input#iinicio')
+    var fim = document.querySelector('input#ifim')
+    var passo = document.querySelector('input#ipasso')
+    var resultado = document.querySelector('div#resultado')
+    for (c=inicio; c<=fim; c++)
+        resultado.a
+}
