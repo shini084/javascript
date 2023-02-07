06f80e40d43405b456fae11220395e7b158bec59
Shini084 <79849892+shini084@users.noreply.github.com>
2023-02-06 21:34:29 -0300

    ex018

--- exercicíos/ex018/ex018-scripty.js
+++ exercicíos/ex018/ex018-scripty.js
@@ -1,8 +1,27 @@
 function contar(){
-    var inicio = window.document.querySelector('input#iinicio')
-    var fim = document.querySelector('input#ifim')
-    var passo = document.querySelector('input#ipasso')
-    var resultado = document.querySelector('div#resultado')
-    for (c=inicio; c<=fim; c++)
-        resultado.a
+    let ini = window.document.querySelector('input#txti')
+    let fim = document.querySelector('input#txtf')
+    let passo = document.querySelector('input#txtp')
+    var res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('[ERRO] Faltam dados!')
+    } else{
+        res.innerHTML = 'contando...'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (i < f){
+            res.innerHTML = ''
+            for(c=i; c<=f; c+=p){
+                res.innerHTML +=  `\u{1F449} ${c} `
+            }
+        }else if (f <= i){             
+            res.innerHTML = ''
+            for(c=i; c>=f; c-=p){
+                res.append(`\u{1F449} ${c} `)
+            }
+        }
+        res.innerHTML += '\u{1F449} \u{1F3C1}'
+    }
 }
