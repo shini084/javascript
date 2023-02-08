9675322ed48cbb7547a1424078e2a40794ff0b7f
Shini084 <79849892+shini084@users.noreply.github.com>
2023-02-08 17:34:12 -0300

    ex019

--- exercicíos/ex019/ex019-scripty.js
+++ exercicíos/ex019/ex019-scripty.js
@@ -1,14 +1,22 @@
 function tabuada() {
     let numtxt = document.querySelector('input#inum')
     let res = document.querySelector('div#resultado')
+    let tab = document.getElementById('seltab')
 
     if (numtxt.value.length == 0){
         window.alert('por favor insira um número')
     } else{
         let n = Number(numtxt.value)
         res.innerHTML = ''
+        tab.innerHTML = ''
         for (c=1; c<=10; c++){
             res.innerHTML += `${n} x ${c} = ${n*c} <br>`
+            let item = document.createElement('option')
+            item.text =  `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
         }
     }
+    
+    
 }
